916fb6f73b4f60019f32ad9e906cb65aeb03fd41
felipe-bit <super.lipe2008@gmail.com>
2025-03-20 14:33:25 -0300

    classe Plano

---
+++ Models/Plano.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GerenciamentoMensalidade.Models
+{
+    [Table("Plano")]
+    public class Plano
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Column("DiasPorSemana")]
+        [Display(Name = "DiasPorSemana")]
+        public string DiasPorSemana { get; set; } = string.Empty;
+
+
+    }
+}
