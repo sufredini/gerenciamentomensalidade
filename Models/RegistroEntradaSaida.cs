dbbfe10c6092a00e866eb76b5ad423e70db1041f
JoWwWw <priscilaejoaovitor831@gmail.com>
2025-03-20 15:09:16 -0300

    Classe RegistroEntradaSaida

---
+++ Models/RegistroEntradaSaida.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace GerenciamentoMensalidade.Models
+{
+    [Table("RegistroEntradaSaida")]
+
+    public class RegistroEntradaSaida
+    {
+        [Column("Id")]
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Column("Entrada")]
+        [Display(Name = "Entrada")]
+        public string Entrada { get; set; } = string.Empty;
+
+
+        [Column("Saida")]
+        [Display(Name = "Saída")]
+        public string Saida { get; set; } = string.Empty;
+
+    }
+}
