30b043e4b8c3a9c05540335eb7917eded8bebd25
vinimoraeess-0 <vinicius027m@gmail.com>
2025-03-20 15:03:00 -0300

    classe pagamento

---
+++ Models/Pagamento.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GerenciamentoMensalidade.Models
+{
+    [Table("Pagamentos")]
+    public class Pagamento
+    {
+        [Column("Id")]
+        [Display(Name = "PagamentoId")]
+        public int Id { get; set; }
+
+        [ForeignKey("ALunosId")]
+        [Display(Name = "Id do Aluno")]
+        public int AlunosId { get; set; }
+        public Alunos? Alunos { get; set; }
+
+        [Column("DataPagamento")]
+        [Display(Name = "DtPagamento")]
+        public string DtPagamento { get; set; } = string.Empty;
+    }
+}
